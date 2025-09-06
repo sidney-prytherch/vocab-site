b1df6a3be406b72623d3e7641c7195b05e271979
sidney-prytherch <sidneyprytherch@gmail.com>
2025-09-06 16:09:06 -0700

    Tentatively fix PWA

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
-
+	import { asset } from '$app/paths';
 	let { children } = $props();
 </script>
 
 <svelte:head>
+	<link rel="manifest" href="{asset('/manifest.json')}">
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
