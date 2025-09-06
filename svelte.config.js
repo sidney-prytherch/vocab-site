90a2d26b1e7f0e7b5dbc00a980892f62bc2f8d08
sidney-prytherch <sidneyprytherch@gmail.com>
2025-09-05 19:06:29 -0700

    Update for github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,19 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+const dev = process.argv.includes('dev');
 
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+/** @type {import('@sveltejs/kit').Config} */
+
+const config = {
+    kit: {
+        // appDir: 'app', // Required as the default is _app
+        adapter: adapter(),
+        paths: {
+            base: dev ? '' : '/vocab-site',
+        }
+    },
+    preprocess: vitePreprocess()
 };
 
 export default config;
